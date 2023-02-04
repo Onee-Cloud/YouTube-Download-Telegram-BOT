fcb9d5f7ef5f1cb7a67573e686a328244966e027
Toyvo <denbut2002@gmail.com>
2023-02-04 04:11:09 +0200

    04.02.23 4:11

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import telebot
 from pytube import YouTube
 from telebot import types
 
-link = ''
-
 bot=telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -16,13 +14,23 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    if 'youtube.com' in message.text:
+    if ('youtube.com' in message.text) or ('youtu.be' in message.text):
+        video = None
         try:
-            YouTube(message.text)
-            bot.send_message(message.chat.id, message.text)
+            video = YouTube(message.text)
         except Exception:
-            bot.send_message(message.chat.id, 'Введіть коректне посилання')
+            bot.send_message(message.chat.id, 'Помилка, перевірте посилання та  спробуйте ще')
+        finally:
+            bot.send_message(message.chat.id, 'Назва відео: {title} \n Кількість переглядів: {views} \n Довжина відео: {length}'.format(title=video.title, views=video.views, length=str(video.length//60) + ' : ' + str(video.length%60)))
+            ys = video.streams.get_highest_resolution()
+            bot.send_message(message.chat.id, 'Завантаження почалось зачекайте будьласка')
+            ys.download(None, 'video.mp4')
+            bot.send_message(message.chat.id, 'Завантаження закінчилось відправляємо вам')
+            vid = open('video.mp4', 'rb')
+            bot.send_video(message.chat.id, vid)
+            vid.close()
 
 
 
-bot.infinity_polling()
+
+bot.polling(none_stop = True)
