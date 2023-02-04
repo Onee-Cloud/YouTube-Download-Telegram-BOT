862baff871cad33f7d25714ee1fa0bc69f7dd8ef
Toyvo <denbut2002@gmail.com>
2023-02-04 02:24:06 +0200

    04.02.23 2:24

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import config
+import pytube
 import telebot
 
+from pytube import YouTube
 from telebot import types
 
-bot = telebot.TeleBot(config.TOKEN)
+link = ''
 
-@bot.message_handler(commands = ['/start'])
+bot=telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, config.GREETING[0])
+    bot.send_message(message.chat.id, config.GREETING[1])
+
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    if 'youtube.com' in message.text:
+        try:
+            YouTube(message.text)
+            bot.send_message(message.chat.id, message.text)
+        except Exception:
+            bot.send_message(message.chat.id, 'Введіть коректне посилання')
+
+
 
 bot.infinity_polling()
